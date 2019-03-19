3ff4945a0161f1adfd7a60c929d37dbb0710e04f
NamagembeDaphine <namagembedaphine@gmail.com>
2019-03-19 18:18:53 +0300

    Update index.php

--- daphine's website/index.php
+++ daphine's website/index.php
@@ -1,1 +1,30 @@
 
+<?php
+         
+         if(isset($_P0ST['submit'])){
+		    if(empty($_P0ST["message"]))
+			{
+			  "please enter your name, email and message"
+			 
+			}else
+			{
+			 $to= "namagembedaphine@gmail.com";
+			 $subject= "form-Tutorial";
+			 $name_field= $_P0ST['first_name'];
+			 $email_field= $_P0ST['email'];
+			 $message= $_P0ST['message'];
+			 $body= "from:$name_field\n E-mail:$email_field\n message:\n $message";
+			 $success= mail($to, $subject, $body);
+			 if($success)
+			 {
+			     $url= "http://www.namagembedaphine@gmail.com";
+				 Header("Location:" .$url)
+			 }else
+			 {
+			     die("message cannot be sent");
+			 }
+			}
+		 }	 
+	
+	
+	     ?>
